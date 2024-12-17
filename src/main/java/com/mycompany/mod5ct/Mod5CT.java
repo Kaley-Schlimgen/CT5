49bec87a1aa4073d6ff2862f1964c7b811df51f8
Kaley-Schlimgen <kaleynicole89@gmail.com>
2024-12-17 12:53:15 -0700

    2

--- src/main/java/com/mycompany/mod5ct/Mod5CT.java
+++ src/main/java/com/mycompany/mod5ct/Mod5CT.java
@@ -8,9 +8,29 @@ package com.mycompany.mod5ct;
  *
  * @author kaleyschlimgen
  */
+import java.util.Scanner;
+
 public class Mod5CT {
 
+    public static int sum(int myNum) {
+        int i;
+       
+        if (myNum == 0) {
+            return 0;
+        } 
+        else {
+            
+            for (i = 1; i <= 5; ++i){
+            System.out.println("Enter number " + i + ": ");
+            return myNum + sum(myNum - 1);
+            }
+        }
+        return 0;
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int number = 5;
+        int result = sum(number);
+        System.out.println("Sum: " + result);
     }
 }
