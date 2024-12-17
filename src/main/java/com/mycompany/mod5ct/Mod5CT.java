0781f05b6d479156a2119603d2c63096aa3617b4
Kaley-Schlimgen <kaleynicole89@gmail.com>
2024-12-17 13:43:21 -0700

    4

--- src/main/java/com/mycompany/mod5ct/Mod5CT.java
+++ src/main/java/com/mycompany/mod5ct/Mod5CT.java
@@ -14,36 +14,28 @@ public class Mod5CT {
     private static int i = 1;
 
     public static int calculateSum(int n, Scanner scanner) {
-        //private static int counter = 1;
-       
-            
+               
         if (n <= 0) {
             return 0;
         } 
+        
         else {
-            //int i;
-            //i = 1;
-            
-            //for (i = 1; i <= 5; i++){
             System.out.print("Enter number " + i + ": ");
             i++;
-                int myNum = scanner.nextInt();
-            
-               //if i don't have below, i increments??
-               //return myNum;
-            
-               return myNum + calculateSum(n - 1, scanner);
-           
-            //}
-            //return 0;
+            int myNum = scanner.nextInt();
+
+            return myNum + calculateSum(n - 1, scanner);
         }
     }
     
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
-
-        int result = calculateSum(5, scanner);
+        
+        int numToAdd = 5;
+        
+        System.out.println("Enter " + numToAdd + 
+                " numbers to be added together:");
+        int result = calculateSum(numToAdd, scanner);
         System.out.println("Sum: " + result);
     }
 }
