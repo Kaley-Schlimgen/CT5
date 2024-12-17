1d7ffc3b0cc818b8adabffb683aa7efc22cb3f56
Kaley-Schlimgen <kaleynicole89@gmail.com>
2024-12-17 12:41:55 -0700

    1

---
+++ src/main/java/com/mycompany/mod5ct/Mod5CT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mod5ct;
+
+/**
+ *
+ * @author kaleyschlimgen
+ */
+public class Mod5CT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
